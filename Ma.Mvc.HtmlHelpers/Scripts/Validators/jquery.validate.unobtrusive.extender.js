9c29eeed331955ebf921f07a26b9e9da5b6d104a
MammadovAdil <mammadov.adil@outlook.com>
2017-10-06 15:14:22 +0400

    Added number method to validator script.

--- Ma.Mvc.HtmlHelpers/Scripts/Validators/jquery.validate.unobtrusive.extender.js
+++ Ma.Mvc.HtmlHelpers/Scripts/Validators/jquery.validate.unobtrusive.extender.js
@@ -10,6 +10,9 @@ $(document).ready(function () {
     setInputValidations();
 });
 
+$.validator.methods.number = function (value, element) {
+    return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:[\s\,]\d{3})+)(?:[\,]\d+)?$/.test(value);
+}
 
 $.validator.setDefaults({
     ignore: []
